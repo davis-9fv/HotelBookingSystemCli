1ca9056d425a009508ea8caf5b65fe3d2c926a88
Elmo Kilkki <elmo.kilkki@hotmail.com>
2018-05-01 17:36:05 +0200

    Add component for normal user hotel list

--- src/app/hotels/hotels.component.ts
+++ src/app/hotels/hotels.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {IHotel} from '../hotel-admin/hotel-admin.component';
 
 @Component({
   selector: 'app-hotels',
@@ -6,10 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hotels.component.css']
 })
 export class HotelsComponent implements OnInit {
+  listHotelsShowHide = 'visible';
+  newModifyShowHide = 'collapse';
+  isNew = true;
+  hotels: IHotel[];
+  hotelTmp;
 
   constructor() { }
 
   ngOnInit() {
+  }
+  checkInformation(hotel) {
+    this.showHotelDetails();
+    this.hotelTmp = Object.create(hotel);
+    console.log(hotel);
+    this.isNew = false;
+  }
+  initHotelTmp() {
+    this.hotelTmp = {
+      phoneNumber: '', name: '', phone: '', address: ''
+      , email: '', numberRooms: 1, description: ''
+    };
+  }
+  searchHotels() {
+  }
+  showHotelDetails() {
+    this.listHotelsShowHide = 'collapse';
+    this.newModifyShowHide = 'visible';
+    this.initHotelTmp();
+    this.isNew = true;
   }
 
 }

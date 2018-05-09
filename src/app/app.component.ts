8494b78c92a081d9432259fe2977ab8126f84132
Francisco <francisco.vinueza@alterbios.com>
2018-05-09 23:15:23 +0200

    Login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {UserService} from "./services/user.service";
+import {Router} from "@angular/router";
+import {IUser} from "./login/login.component";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  currentUser: IUser;
+
+
+  constructor() {
+    console.log("App.Componenet:")
+    try {
+      var currentUser = localStorage.getItem('currentUser')
+      console.log("App.Componenet:" + currentUser)
+      this.currentUser = JSON.parse(currentUser);
+      console.log("App.Componenet:" + this.currentUser.username)
+      this.title = this.currentUser.username
+    } catch (e) {
+      console.log("App.Componenet: No signed in" + e)
+      this.title = 'No signed in';
+    }
+
+  }
+
 }
